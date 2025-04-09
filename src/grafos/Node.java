2b9ea43fc3f55007ff7d6cbd08322f0306b7408b
sofiac1024 <sofiacastro1024@gmail.com>
2025-04-09 20:03:08 +0200

    gg2

--- src/grafos/Node.java
+++ src/grafos/Node.java
@@ -1,5 +1,8 @@
 package grafos;
 
+import java.util.function.Consumer;
+
 public class Node<S> {
-	
+	private Consumer<S> codigo;
+	private Node<S> nextNode;
 }
--- src/grafos/StateGraph.java
+++ src/grafos/StateGraph.java
@@ -1,16 +1,21 @@
 package grafos;
 
 import java.util.HashMap;
+import java.util.function.Consumer;
 
 public class StateGraph<T> {
 	private String nombre;
 	private String desc;
 	private HashMap<String, Node<T>> nodos = new HashMap<>();
+	private Node<T> nodoI;
+	private Node<T> nodoF;
 	
 	public StateGraph(String nombre, String desc) {
 		this.nombre = nombre;
 		this.desc = desc;
 	}
 	
-	
+	public StateGraph<T> addNode(String nombreNodo, Consumer<T> codNodo) {
+		return this;
+	}
 }
