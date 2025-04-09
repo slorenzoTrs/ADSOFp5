2a8e0fb18768b85a4b2004bfbf252329652758c6
sofiac1024 <sofiacastro1024@gmail.com>
2025-04-09 19:30:58 +0200

    stateGraph init

---
+++ src/grafos/StateGraph.java
@@ -0,0 +1,11 @@
+package grafos;
+
+public class StateGraph<T> {
+	private String nombre;
+	private String desc;
+	
+	public StateGraph(String nombre, String desc) {
+		this.nombre = nombre;
+		this.desc = desc;
+	}
+}
