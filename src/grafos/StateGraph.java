1c017b10e0bb53ecdb5a2376603af96aeafe275a
sofiac1024 <sofiacastro1024@gmail.com>
2025-04-09 19:43:32 +0200

    añadir nodo

--- src/grafos/StateGraph.java
+++ src/grafos/StateGraph.java
@@ -1,11 +1,16 @@
 package grafos;
 
+import java.util.HashMap;
+
 public class StateGraph<T> {
 	private String nombre;
 	private String desc;
+	private HashMap<String, Node<T>> nodos = new HashMap<>();
 	
 	public StateGraph(String nombre, String desc) {
 		this.nombre = nombre;
 		this.desc = desc;
 	}
+	
+	
 }
