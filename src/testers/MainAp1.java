94ecaad1c406e5b83589f5aee2fdb494e9695ffc
sofiac1024 <sofiacastro1024@gmail.com>
2025-04-23 18:40:59 +0200

    MainAp1 documentado

--- src/testers/MainAp1.java
+++ src/testers/MainAp1.java
@@ -2,7 +2,24 @@ package testers;
 
 import grafos.StateGraph;
 
+/**
+ * La clase MainAp1 actúa como tester del apartado 1 de la práctica para probar un flujo de 
+ * operaciones matemáticas implementado mediante un grafo de estados (StateGraph).
+ * Crea un flujo que suma dos números y luego eleva el resultado al cuadrado.
+ * 
+ * Ejecuta el flujo con un conjunto de datos numéricos como entrada y muestra el resultado.
+ * 
+ * @author Sofía Castro - sofiai.castro@estudiante.uam.es
+ * @author Sara Lorenzo - sara.lorenzot@estudiante.uam.es
+ * Pareja 11
+ */
 public class MainAp1 {
+	
+	/**
+     * Método principal que lanza la ejecución del grafo de estados con datos de entrada específicos.
+     * 
+     * @param args Argumentos de línea de comandos (no utilizados).
+     */
 	public static void main(String[] args) {
 		StateGraph<NumericData> sg = buildWorkflow();
 		
@@ -13,6 +30,13 @@ public class MainAp1 {
 		NumericData output = sg.run(input, true);
 		System.out.println("result = " + output);
 	}
+	
+	/**
+     * Construye un flujo de trabajo representado por un grafo de estados,
+     * donde se realiza la suma de dos números y se eleva al cuadrado el resultado.
+     * 
+     * @return El grafo de estados configurado para realizar las operaciones matemáticas.
+     */
 	private static StateGraph<NumericData> buildWorkflow() {
 		StateGraph<NumericData> sg = new StateGraph("math2", "Add two numbers, and then square");
 		
